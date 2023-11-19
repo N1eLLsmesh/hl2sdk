da2fbe219eaf1aa23af59f6c86cd0e7f1c1b8e89
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-19 23:36:46 +0500

    Update BaseAnimatingOverlay.h

--- game/server/BaseAnimatingOverlay.h
+++ game/server/BaseAnimatingOverlay.h
@@ -1,4 +1,4 @@
-//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+//========= Copyright Â© 1996-2005, Valve Corporation, All rights reserved. ============//
 //
 // Purpose: 
 //
@@ -16,7 +16,7 @@
 
 class CBaseAnimatingOverlay;
 
-class CAnimationLayer : public CMemZeroOnNew
+class CAnimationLayer /*: public CMemZeroOnNew*/
 {
 public:	
 	DECLARE_CLASS_NOBASE( CAnimationLayer );
